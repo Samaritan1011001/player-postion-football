a82ad7bb23a7c55f5d4a45e0e48458caa8842a84
Manoj <manojnb95@gmail.com>
2020-01-06 01:33:30 -0600

    Normalize float columns

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from collections import defaultdict
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt  # Plotting
-
+import numpy as np
 import sys
 from impyute.imputation.cs import fast_knn
 sys.setrecursionlimit(100000) #Increase the recursion limit of the OS
@@ -44,18 +44,82 @@ def collect():
 
 def main():
     # collect(conn)
-    player_position_df = pd.read_csv("data/final_data.csv")
+    player_position_df = pd.read_csv("data/no_ids_data.csv")
     player_position_df = player_position_df.dropna(axis=0, subset=['Position'])
-    print(f"player_position_df -> {player_position_df}")
-    print(f'features -> {player_position_df.columns}')
-    drop_list = ["index","id",'player_fifa_api_id', 'player_api_id','date',]
-    player_position_df = player_position_df.drop(drop_list,axis=1)
-    print(f"player_position_df -> {player_position_df}")
 
+    print(f"player_position_df -> {player_position_df}")
+    player_position_df = rearrange_columns(player_position_df)
+    print(f'features -> {player_position_df.columns}')
+    # print(f'{player_position_df.overall_rating.max()}')
+    # plot_attr_distribution(player_position_df,"graphs")
+
+    # player_position_df = drop_unwanted_columns(player_position_df)
     # data_stats(player_position_df=player_position_df)
     # impute_object_columns(player_position_df)
     # impute_float_columns(player_position_df)
     # player_position_df.to_csv("data/no_ids_data.csv",index=False)
+
+def rearrange_columns(player_position_df):
+    rearranged_columns = [
+
+        # Attacking attributes
+        'attacking_work_rate',
+        'crossing', 'finishing',
+        'volleys', 'dribbling', 'curve', 'free_kick_accuracy',
+        'long_passing', 'ball_control', 'acceleration', 'sprint_speed','agility', 'reactions',  'shot_power',
+        'long_shots', 'penalties',
+
+        # Defensive attributes
+        'defensive_work_rate', 'jumping', 'strength', 'interceptions', 'marking',
+        'stamina', 'vision',
+        'standing_tackle', 'sliding_tackle', 'gk_diving', 'gk_handling', 'gk_kicking', 'gk_positioning', 'gk_reflexes',
+
+        # Mixed attributes
+        'overall_rating', 'aggression', 'balance', 'positioning','heading_accuracy','short_passing', 'potential',
+        'preferred_foot', 'Position']
+    return player_position_df[rearranged_columns]
+
+def plot_attr_distribution(player_position_df,graph_directory):
+    fig, ax = plt.subplots()
+    df_new_ST = player_position_df[player_position_df['Position'] == 'ST'].iloc[::200, :-1]
+    df_new_ST = df_new_ST.select_dtypes(float)
+    cols = [col for col in df_new_ST.columns if col not in ['Position']]
+    df_new_ST = df_new_ST.div(df_new_ST.sum(axis=1), axis=0)
+    print(f'Final df plotted-> {df_new_ST}')
+    df_new_ST.T.plot.line(color='black', figsize=(15, 15), legend=False, ylim=(0, 0.08),
+                          title="ST's attributes distribution", ax=ax)
+
+    ax.set_xlabel('Attributes')
+    ax.set_ylabel('Rating')
+
+    ax.set_xticks(np.arange(len(cols)))
+    ax.set_xticklabels(labels=cols, rotation=90)
+
+    for ln in ax.lines:
+        ln.set_linewidth(1)
+
+    ax.axvline(0, color='red', linestyle='--')
+    ax.axvline(15, color='red', linestyle='--')
+
+    ax.axvline(16, color='blue', linestyle='--')
+    ax.axvline(29, color='blue', linestyle='--')
+
+    ax.axvline(30, color='green', linestyle='--')
+    ax.axvline(51, color='green', linestyle='--')
+
+    ax.text(5, 100, 'Attack Attributes', color='red', weight='bold')
+    ax.text(17, 100, 'Defend Attributes', color='blue', weight='bold')
+    ax.text(33, 100, 'Mixed Attributes', color='green', weight='bold')
+
+    plt.savefig(graph_directory + "/line_plot_after_norm.png")
+    plt.show()
+
+
+def drop_unwanted_columns(player_position_df):
+    drop_list = ["index","id",'player_fifa_api_id', 'player_api_id','date',]
+    player_position_df = player_position_df.drop(drop_list,axis=1)
+    print(f"player_position_df -> {player_position_df}")
+    return player_position_df
 
 
 def data_stats(player_position_df):
